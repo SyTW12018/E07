38ea941b9a56079f33b5320d53d90ffa5bc72b98
alu0100799101 <alu0100799101@ull.edu.es>
2018-11-11 19:47:27 +0000

    Añadido post.js

---
+++ gamerin/server/models/post.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var PostSchema = new mongoose.Schema({
+	body: {
+	type: String,
+	minlength: 10,
+	maxlength: 260
+	},
+	createAt: Date ,
+	postType:{
+	required: true,
+	type: String
+	},
+	comments: [{
+	type: mongoose.Schema.Types.ObjectId,
+	ref: 'Comment'
+	}],
+	shares: [{
+	type: mongoose.Schema.Types.ObjectId,
+	ref: 'Post'
+	}],
+	tags: [{
+	type: mongoose.Schema.Types.ObjectId,
+	ref: 'Tag'
+	}],
+	likes: [{
+	type: mongoose.Schema.Types.ObjectId,
+	ref: 'Like'
+	}]
+	});
+module.exports = mongoose.model("Post", PostSchema);
