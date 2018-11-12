ee9731ae7cc8bace67202e9cbc82a187b94ce477
Alejandro Gonzalez Alonso <alu0100997910@ull.edu.es>
2018-11-12 09:57:53 +0000

    added express server base

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let express = require('express');
+
+let app = express();
+
+
+//Rutas
+
+
+//Cabeceras HTTP
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE');
+    res.header('Allow','GET,POST,OPTIONS,PUT,DELETE');
+    next();
+});
+
+
+module.exports = app;
