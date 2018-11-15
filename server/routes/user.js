1badd3a922477ff8ab73d5458165eb9e83a7e181
Alejandro Gonzalez Alonso <alu0100997910@ull.edu.es>
2018-11-15 12:13:57 +0000

    refactor

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,8 +3,6 @@
 let express = require('express');
 let UserController = require('../controllers/user');
 
-let mdw_auth = require('../middlewares/authentication');
-
 let api = express.Router();
 
 api.post('/register', UserController.registerUser);
