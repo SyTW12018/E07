7ba6285c10e386726459a9a7b818357c60af13cc
Adrian Herrera Darias <alu0100799101@ull.edu.es>
2018-11-15 12:21:46 +0000

    Añadido en routes , post

---
+++ server/routes/post.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let express = require('express');
+let PostController = require('../controller/post');                                                                                                                                                                                                                                                                                                                                                                                                                  
+
+let mdw_auth = require('../middlewares/authentication');
+
+let api = express.Router();
+
+api.post('/addpost', PostController.addpost);
+api.post('/getpost/:id', PostController.getpost);
+
+module.exports = api;
