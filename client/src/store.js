a71eee16e1f43b565eaca566fa0213003ae81b87
Lucia Muñoz González <alu0100949193@ull.edu.es>
2018-12-30 16:33:13 +0000

    añadida funcionalidad de login

--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,48 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    name: "adf",
+    surname: "",
+    username: "",
+    email: "",
+    token: ""
   },
   mutations: {
-
+    setName(state, payload) {
+      state.name = payload;
+    },
+    setSurname(state, payload) {
+      state.surname = payload;
+    },
+    setUsername(state, payload) {
+      state.username = payload;
+    },
+    setEmail(state, payload) {
+      state.email = payload;
+    },
+    setToken(state, payload) {
+      state.token = payload;
+    }
   },
   actions: {
+
+  },
+  getters: {
+    name(state) {
+      return state.name;
+    },
+    surname(state) {
+      return state.surname;
+    },
+    email(state) {
+      return state.email;
+    },
+    token(state) {
+      return state.token;
+    },
+    username(state) {
+      return state.username;
+    }
 
   }
 })
